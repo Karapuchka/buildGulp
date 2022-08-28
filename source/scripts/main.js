de192e674069d2d94598473ce36c250dd745e378
Karapuchka <77234090+Karapuchka@users.noreply.github.com>
2022-08-28 20:19:20 +0300

    Delete source directory

--- source/scripts/main.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-const formSearch = document.forms.formSearch
-
-formSearch.addEventListener('click', function(event){
-    if(event.target.closest('.form-search__btn')){
-        event.preventDefault();
-    }
-})
